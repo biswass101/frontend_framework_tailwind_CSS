057c6ba58acc95d4e79e44ab12a358dde2fdd3b3
biswass101 <biswassnaeemcse@gmail.com>
2024-05-22 18:02:14 +0600

    basic tailwind lessons with full installation

---
+++ project-1/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+
+      screens : 
+      {
+        'niloy1200' : '1200px'
+      }
+    },
+  },
+  plugins: [],
+}
+
